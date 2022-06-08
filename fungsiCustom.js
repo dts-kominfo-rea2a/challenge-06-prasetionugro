344596320fc6efeafb48b99567fbffff5340a525
Prasetio Nugroho <prasetio.nugro@gmail.com>
2022-06-08 11:15:59 +0700

    feat: Menambah fungsi bacaData try 01

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,41 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = function (fnCallback) {
+  let result = [];
+
+  fs.readFile(file1, "utf-8", (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    }
+
+    let dataParsed = JSON.parse(data);
+    let kataSplit = dataParsed["message"].split(" ");
+    result.push(kataSplit[1]);
+
+    fs.readFile(file2, "utf-8", (err, data) => {
+      if (err) {
+        return fnCallback(err, null);
+      }
+
+      let dataParsed = JSON.parse(data);
+      let kataSplit = dataParsed[0]["message"].split(" ");
+      result.push(kataSplit[1]);
+
+      fs.readFile(file3, "utf-8", (err, data) => {
+        if (err) {
+          return fnCallback(err, null);
+        }
+
+        let dataParsed = JSON.parse(data);
+        let kataSplit = dataParsed[0]["data"]["message"].split(" ");
+        result.push(kataSplit[1]);
+
+        return fnCallback(null, result);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
